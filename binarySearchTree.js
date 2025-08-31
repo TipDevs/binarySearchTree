ece193285b8168356ca39791235bf376916724d0
TipDevs <tipdevs@gmail.com>
2025-08-31 01:47:26 +0100

    feat: Add insert() and delete() methods and implement a private method for each in Tree class function that can not be accessed by external access

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -22,4 +22,47 @@ class Tree {
     root.right = this.#buildTree(arr, mid + 1, end);
     return root;
   }
+  insert(value) {
+    this.root = this.#insert(this.root, value);
+  }
+  #insert(root, value) {
+    // Private method implemented to insert new Node to the tree behind the scene and prevent external access
+    if (root === null) return new Node(value);
+    if (root.data === value) return root;
+    if (value < root.data) root.left = this.#insert(root.left, value);
+    else if (value > root.data)
+      root.right = this.#insert(root.right, value);
+    return root;
+  }
+  deleteItem(value) {
+    this.root = this.#deleteItem(this.root, value);
+  }
+  #deleteItem(root, value) {
+    // Private method implemented to delete a node from the tree behind the scene and prevent external access
+    let current = root;
+    let prev = null;
+    while (current !== null && current.data !== value) {
+      prev = current;
+      if (value < current.data) current = current.left;
+      else current = current.right;
+    }
+    if (current === null) return root;
+    if (current.left === null || current.right === null) {
+      let newCurrent = current.left === null ? current.right : current.left;
+      if (prev === null) return newCurrent;
+      if (current === prev.left) prev.left = newCurrent;
+      else prev.right = newCurrent;
+    } else {
+      let prev = null;
+      let tempVar = current.right;
+      while (tempVar.left !== null) {
+        prev = tempVar;
+        tempVar = tempVar.left;
+      }
+      if (prev !== null) p.left = tempVar.right;
+      else current.right = tempVar.right;
+      current.data = tempVar.data;
+    }
+    return root;
+  }
 }
