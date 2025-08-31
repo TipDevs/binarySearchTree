fad310ed8afb92953c6ac916accd59c7db33f3c9
TipDevs <tipdevs@gmail.com>
2025-08-31 02:13:24 +0100

    fix: maximum call stack exceeded bug fixed

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -9,17 +9,21 @@ class Tree {
   constructor(array) {
     this.root = this.#buildTree(array);
   }
-  buildTree(array) {
+  #buildTree(array) {
     const uniqueSortedArray = [...new Set(array)].sort((a, b) => a - b);
-    return this.#buildTree(uniqueSortedArray, 0, uniqueSortedArray.length - 1);
+    return this.#buildTreeRecur(
+      uniqueSortedArray,
+      0,
+      uniqueSortedArray.length - 1
+    );
   }
-  #buildTree(arr, start, end) {
+  #buildTreeRecur(arr, start, end) {
     // Private method implemented to build tree behind the scene and prevent external access
     if (start > end) return null;
     let mid = start + Math.floor((end - start) / 2);
     let root = new Node(arr[mid]);
-    root.left = this.#buildTree(arr, start, mid - 1);
-    root.right = this.#buildTree(arr, mid + 1, end);
+    root.left = this.#buildTreeRecur(arr, start, mid - 1);
+    root.right = this.#buildTreeRecur(arr, mid + 1, end);
     return root;
   }
   insert(value) {
